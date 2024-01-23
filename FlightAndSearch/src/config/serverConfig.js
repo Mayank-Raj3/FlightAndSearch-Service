ddb0c6fa5f7c8960dee8e41a169290cd750ffac0
Mayank-Raj3 <guptamannu884@gmail.com>
2024-01-23 14:51:19 +0530

    Initial setup And dotenv

---
+++ FlightAndSearch/src/config/serverConfig.js
@@ -0,0 +1,8 @@
+//  we are using common js moduling its by default
+
+const dotenv = require(dotenv);
+dotenv.config();
+
+module.exports = {
+  PORT: process.env.port,
+};
---
+++ FlightAndSearch/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
+
+const setupAndStartServer = async () => {
+  const app = express();
+  app.listen(PORT, () => {
+    console.log(`Server Started at ${PORT}`);
+    console.log(process.env);
+  });
+};
+
+setupAndStartServer();
